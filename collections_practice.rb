508a9dd93b72ab4b4ce63cc62e3ff072973eeaf7
Luca Kanyo <lucakanyo@protonmail.com>
2020-05-28 01:32:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,15 +29,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1..2].sort do |a, b| 
-    if a > b || a == b || a < b 
-      1 
-    end 
-    array
-  end 
+  array[1], a[2] = a[2], a[1]
+  array
 end 
-
-
 
 
 def reverse_array(array)
